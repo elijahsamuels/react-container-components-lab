05a68a9240036fe24939669e571112aaa8723487
Elijah Samuels <elijahsamuels@gmail.com>
2021-03-24 15:01:33 -0600

    commit tree d40ac7f25232cd06b8d5ea1d44d8142293d52c94

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,28 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+
+export default class LatestMovieReviewsContainer extends Component {
+
+    state = {
+        reviews: []
+    }
+
+    componentDidMount() {
+        fetch(URL)
+        .then(response => response.json())
+        .then(data => this.setState({reviews: data.results}))
+        // console.log('data.results', data )
+    }
+
+    render() {
+        return (
+        <div className="latest-movie-reviews">
+             <div>This is Latest Movie Reviews Container!</div>
+             <MovieReviews reviews={this.state.reviews} />
+        </div>
+    )}
+
+  }
+  
